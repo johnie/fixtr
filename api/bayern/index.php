638da38eded45d4771f1eee40d0281003c2e4852
Johnie Hjelm <johnie.hjelm@symbio.com>
2014-08-25 22:55:16 +0200

    Made the api micro hypermedia

--- api/bayern/index.php
+++ api/bayern/index.php
@@ -1,5 +1,5 @@
 <?php
   $bayern = file_get_contents("../../bayern.json");
-  header('Content-type: application/json');
+  header('Content-type: application/json; charset=utf-8');
   echo $bayern;
 ?>
--- api/chelsea/index.php
+++ api/chelsea/index.php
@@ -1,5 +1,5 @@
 <?php
   $chelsea = file_get_contents("../../chelsea.json");
-  header('Content-type: application/json');
+  header('Content-type: application/json; charset=utf-8');
   echo $chelsea;
 ?>
